790c31c0c2a85f5c9855afd7e36c382a7c897de5
Shumzzzz <61364206+Shumzzzz@users.noreply.github.com>
2020-08-25 20:56:15 +0400

    1

--- js/time.js
+++ js/time.js
@@ -1,8 +1,17 @@
 setInterval(function showtime() {
     let now = new Date();
     let hours = now.getHours();
+    if(hours < 10) {
+        hours = `0${hours}`
+    }
     let minutes = now.getMinutes();
+    if(minutes < 10) {
+        minutes = `0${minutes}`
+    }
     let seconds = now.getSeconds();
+    if(seconds < 10) {
+        seconds = `0${seconds}`
+    }
     let time = `${hours}:${minutes}:${seconds}`
     document.getElementById("time-here").innerHTML = time;
-}, 1000)
+}, 500)
