06066b5fcc9e536dd71913d0b6ba228acb5302fa
Shumzzzz <61364206+Shumzzzz@users.noreply.github.com>
2020-11-08 15:43:35 +0400

    1

--- js/city.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    country = ymaps.geolocation.country;
-    region = ymaps.geolocation.region;
-    city = ymaps.geolocation.city;
-    userGeo = `${country}, ${region}, ${city}`
-    document.getElementById("userGeo").innerHTML = userGeo;   
-}
